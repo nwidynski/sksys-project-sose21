99903c456cb584a2f3a97895188bf0da276a3466
david <82031094+DavWe@users.noreply.github.com>
2021-06-01 20:22:40 +0200

    add vue-router and bootstrap-vue

---
+++ src/client/router/index.ts
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Profile from '../views/Profile.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
