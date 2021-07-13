dca48c997738a71f6195d2ed5a1f3c1eb1f55511
zlatinametodieva <64097972+zlatinametodieva@users.noreply.github.com>
2021-07-13 19:31:13 +0200

    recipe rating endpoint, remove saved recipe endpoint, get returns now only public recipes (habe es mit Ronen besprochen)

--- src/server/controller/recipe.ts
+++ src/server/controller/recipe.ts
@@ -10,19 +10,16 @@ import { ValidationMessages } from "@server/common/enums/validationMessages.enum
  *
  * @param user
  * @param name
- * @param rating
  * @param isPrivate
  * @return {*}
  */
 const createRecipe = (
   user: User,
   name: string,
-  rating: number = 0,
   isPrivate: boolean = true
 ) => {
   return Prisma.validator<Prisma.RecipeCreateInput>()({
     name,
-    rating,
     isPrivate,
     User: {
       connect: {
@@ -36,24 +33,21 @@ const createRecipe = (
  * Updates an existing recipe.
  *
  * @param name
- * @param rating
  * @param isPrivate
  * @return {*}
  */
 const updateRecipe = (
   name: string,
-  rating: number = 0,
   isPrivate: boolean = true
 ) => {
   return Prisma.validator<Prisma.RecipeUpdateInput>()({
     name,
-    rating,
     isPrivate,
   });
 };
 
 /**
- * Saves the recipe to an user.
+ * Saves the recipe to the user.
  *
  * @param user
  * @return {*}
@@ -64,6 +58,41 @@ const saveRecipe = (
   return Prisma.validator<Prisma.RecipeUpdateInput>()({
     userProfiles: {
       connect: { id: user.id },
+    }
+  });
+};
+
+/**
+ * Removes the saved recipe from the user.
+ *
+ * @param user
+ * @return {*}
+ */
+const removeSavedRecipe = (
+    user: User
+) => {
+  return Prisma.validator<Prisma.RecipeUpdateInput>()({
+    userProfiles: {
+      disconnect: { id: user.id },
+    }
+  });
+};
+
+/**
+ * Updates the rating of the recipe.
+ *
+ * @param rating
+ * @param reviewer
+ * @return {*}
+ */
+const rateRecipe = (
+    rating: number,
+    reviewer: User
+) => {
+  return Prisma.validator<Prisma.RecipeUpdateInput>()({
+    rating,
+    reviewers: {
+      connect: { id: reviewer.id },
     }
   });
 };
@@ -94,12 +123,6 @@ namespace RecipeController {
         .withMessage(ValidationMessages.WRONG_TYPE)
         .trim()
         .escape(),
-      body("rating", ValidationMessages.UNDEFINED)
-        .exists()
-        .isInt()
-        .withMessage(ValidationMessages.WRONG_TYPE)
-        .isInt({ min: 0, max: 5 })
-        .withMessage(ValidationMessages.WRONG_VALUE),
       body("isPrivate", ValidationMessages.UNDEFINED)
         .exists()
         .isBoolean()
@@ -108,7 +131,22 @@ namespace RecipeController {
   };
 
   /**
-   * List all todos
+   * Validates body payload for rating update.
+   */
+  export const validateBodyRate = () => {
+    return [
+      body("rating", ValidationMessages.UNDEFINED)
+          .exists()
+          .isInt()
+          .withMessage(ValidationMessages.WRONG_TYPE)
+          .isInt({ min: 0, max: 5 })
+          .withMessage(ValidationMessages.WRONG_VALUE)
+    ];
+  };
+
+  /**
+   * Returns all public recipes.
+   *
    * @return JSON Array
    */
   export const list = async (
@@ -117,10 +155,8 @@ namespace RecipeController {
     next: NextFunction
   ) => {
     try {
-      const user = req.user;
-
       const recipes = await prisma.recipe.findMany({
-        where: { OR: [{ User: user }, { isPrivate: false }] },
+        where: { isPrivate: false }
       });
 
       res.status(200).json(recipes);
@@ -169,14 +205,14 @@ namespace RecipeController {
     try {
       const user = req.user;
       const { id } = req.params;
-      const { name, rating, isPrivate } = req.body;
+      const { name, isPrivate } = req.body;
 
       const recipe = await prisma.recipe.updateMany({
         where: {
           id,
           User: user,
         },
-        data: updateRecipe(name, rating, isPrivate),
+        data: updateRecipe(name, isPrivate),
       });
 
       res.status(200).json(recipe);
@@ -196,10 +232,10 @@ namespace RecipeController {
   ) => {
     try {
       const user = req.user;
-      const { name, rating, isPrivate } = req.body;
+      const { name, isPrivate } = req.body;
 
       const recipe = await prisma.recipe.create({
-        data: createRecipe(user, name, rating, isPrivate),
+        data: createRecipe(user, name, isPrivate),
       });
 
       res.status(200).json(recipe);
@@ -235,6 +271,100 @@ namespace RecipeController {
         });
 
         res.status(200).json(recipe);
+      }
+    } catch (err) {
+      return next(err);
+    }
+  };
+
+  /**
+   * Removes a specified saved recipe from an existing user.
+   *
+   * @return JSON Object
+   */
+  export const removeSaved = async (
+      req: Request,
+      res: Response,
+      next: NextFunction
+  ) => {
+    try {
+      const user = req.user;
+      const { id } = req.params;
+
+      const recipe = await prisma.recipe.findUnique({
+        where: { id },
+      });
+
+      if (recipe == null) {
+        res.status(404).json("Recipe not found.");
+      } else {
+        await prisma.recipe.update({
+          where: { id },
+          data: removeSavedRecipe(user),
+        });
+
+        res.status(200).json(recipe);
+      }
+    } catch (err) {
+      return next(err);
+    }
+  };
+
+  /**
+   * Calculates and updates the rating of a specified recipe.
+   *
+   * @return JSON Object
+   */
+  export const rate = async (
+      req: Request,
+      res: Response,
+      next: NextFunction
+  ) => {
+    try {
+      const reviewer = req.user;
+      const { id } = req.params;
+      const { rating } = req.body;
+
+      const recipe = await prisma.recipe.findUnique({
+        where: { id },
+        include: {
+          reviewers: {
+            select: { id: true },
+          },
+        },
+      });
+
+      if (recipe == null) {
+        res.status(404).json("Recipe not found.");
+      }
+      else if (reviewer.id == recipe.userId) {
+        res.status(403).json("You cannot rate your own recipe!")
+      }
+      else {
+        let reviewers: Array<string> = [];
+        recipe.reviewers.forEach(extractID);
+
+        function extractID(reviewer: any) {
+          for (let key in reviewer) {
+            if (reviewer.hasOwnProperty(key)) {
+              reviewers.push(reviewer[key]);
+            }
+          }
+        }
+
+        if (reviewers.includes(reviewer.id)) {
+          res.status(403).json("You have already rated this recipe!");
+        }
+        else {
+          const newRating = (recipe.rating * reviewers.length + rating) / (reviewers.length + 1);
+
+          const rated = await prisma.recipe.update({
+            where: { id },
+            data: rateRecipe(parseFloat(newRating.toFixed(2)), reviewer),
+          });
+
+          res.status(200).json(rated);
+        }
       }
     } catch (err) {
       return next(err);
