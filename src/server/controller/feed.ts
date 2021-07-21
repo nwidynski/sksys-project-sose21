91725069e06301fd5fdbf846468e87926dd64902
Nikolas Schröter <nikolas.schroeter@widynski.technology>
2021-07-21 21:40:31 +0200

    Added search and orderBy

--- src/server/controller/feed.ts
+++ src/server/controller/feed.ts
@@ -3,48 +3,82 @@ import { Request, Response, NextFunction } from "express";
 import prisma from "@server/common/services/prisma.service";
 
 namespace FeedController {
-    /**
-     * Returns a list with all public recipes and all meetups.
-     *
-     * @return JSON Array
-     */
-    export const list = async (
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ) => {
-        try {
-            const meetups = await prisma.meetUp.findMany({
-                orderBy: {
-                    createdAt: "desc",
-                },
-                include: {
-                    guests: {
-                        select: {
-                            id: true,
-                            firstname: true,
-                            surname: true,
-                        },
-                    },
-                },
-            });
+  /**
+   * Returns a list with all public recipes and all meetups.
+   *
+   * @return JSON Array
+   */
+  export const list = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const { orderBy, search } = req.query;
 
-            const recipes = await prisma.recipe.findMany({
-                where: { isPrivate: false },
-                orderBy: {
-                    createdAt: "desc",
-                },
-            });
+      const sortOrder = orderBy?.toString() === "asc" ? "asc" : "desc";
 
-            let feed: Array<any> = [];
-            feed.push(meetups);
-            feed.push(recipes);
+      const isIngredientSearch = search
+        ?.toString()
+        .includes("recipe-feature: ");
 
-            res.status(200).json(feed);
-        } catch (err) {
-            return next(err);
-        }
-    };
+      const searchedIngredients =
+        isIngredientSearch && search?.toString().split(" ");
+
+      const meetups = await prisma.meetUp.findMany({
+        ...(search && {
+          where: {
+            OR: [
+              { recipeName: { contains: search.toString() } },
+              { hostName: { contains: search.toString() } },
+            ],
+          },
+        }),
+        orderBy: {
+          createdAt: sortOrder,
+        },
+        include: {
+          guests: {
+            select: {
+              id: true,
+              firstname: true,
+              surname: true,
+            },
+          },
+        },
+      });
+
+      let recipes = await prisma.recipe.findMany({
+        where: {
+          isPrivate: false,
+          ...(search &&
+            !isIngredientSearch && { name: { contains: search?.toString() } }),
+        },
+        orderBy: {
+          createdAt: sortOrder,
+        },
+        include: { Ingredients: true },
+      });
+
+      if (searchedIngredients) {
+        recipes = recipes.filter((recipe) =>
+          recipe.Ingredients.find((ingredient) =>
+            searchedIngredients.find(
+              (searchedIngredient) => searchedIngredient === ingredient.name
+            )
+          )
+        );
+      }
+
+      let feed: Array<any> = [];
+      feed.push(meetups);
+      feed.push(recipes);
+
+      res.status(200).json(feed);
+    } catch (err) {
+      return next(err);
+    }
+  };
 }
 
 export default FeedController;
